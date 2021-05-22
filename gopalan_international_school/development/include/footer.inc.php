8f3bb180c1ff00cd598b440b2f0070206aa80acc
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-22 23:27:17 +0530

    Create footer.inc.php

---
+++ gopalan_international_school/development/include/footer.inc.php
@@ -0,0 +1,250 @@
+
+        <div class="body__footer pt-5">
+            <footer class="wrap__container">
+                <div class="footer__top">
+                    <div class="row">
+                        <div class="col-12 col-md-3">
+                            <div class="footer__brand">
+                                <a href="index.html">
+                                    <img src="img/brand/gopalan_international_school_footer.svg" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            <ul class="d-flex">
+                                <li></li>
+                                <li>
+                                    Get Appointment
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            <ul class="d-flex">
+                                <li></li>
+                                <li>
+                                    Contact Us Today
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            
+                            <ul class="d-flex">
+                                <li></li>
+                                <li>
+                                    Take a School Tour
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- strat footer middle -->
+                <div class="footer__middle">
+                    <div class="row">
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <article class="footer__middle-block">
+                                <header class="footer__header">
+                                    Quick Links
+                                </header>
+                                <ul>
+                                    <li class="footer__list"><a href="" class="footer__link">Home</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">About Us</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">FAQ</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">Testimonial</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">Contact Us</a></li>
+                                </ul>
+                            </article>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <article class="footer__middle-block">
+                                <header class="footer__header">
+                                    Programmes
+                                </header>
+                                <ul>
+                                    <li class="footer__list"><a href="" class="footer__link">IGCSE</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">ISC</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">ICSE</a></li>
+                                </ul>
+                            </article>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <article class="footer__middle-block">
+                                <header class="footer__header">
+                                    Quick Links
+                                </header>
+                                <ul>
+                                    <li class="footer__list"><a href="" class="footer__link">Campus Life</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">Admission</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">Events</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">News</a></li>
+                                    <li class="footer__list"><a href="" class="footer__link">Blogs</a></li>
+                                </ul>
+                            </article>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <article class="footer__middle-block">
+                                <header class="footer__header">
+                                    Contact Info
+                                </header>
+                                <ul class="d-flex pb-3">
+                                    <li class="footer__list pr-3">
+                                        <!-- <svg version="1.1" id="Capa_1" height="20px" class="footer__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                                viewBox="0 0 511.874 511.874" style="enable-background:new 0 0 511.874 511.874;" xml:space="preserve">
+                                                <g>
+                                                    <g>
+                                                        <g>
+                                                            <path d="M490.114,12.813H132.568c-12.012,0.014-21.746,9.748-21.76,21.76v98.62l-16.35-24.525
+                                                                c-7.484-11.289-22.535-14.676-34.133-7.68l-33.638,20.224c-11.016,6.464-19.097,16.946-22.545,29.244
+                                                                c-12.271,44.681-3.166,121.66,109.824,234.667C203.821,474.885,270.816,499.06,316.99,499.06
+                                                                c10.69,0.049,21.339-1.34,31.659-4.13c12.293-3.448,22.775-11.518,29.252-22.519l20.198-33.673
+                                                                c6.968-11.589,3.584-26.609-7.68-34.091l-80.546-53.692c-11.049-7.308-25.859-4.905-34.031,5.521l-23.45,30.148
+                                                                c-2.451,3.226-6.897,4.166-10.445,2.21l-4.463-2.458c-14.686-8.004-32.964-17.971-69.879-54.886
+                                                                c-3.994-3.994-7.612-7.731-11.008-11.307h333.517c11.982,0.009,21.713-9.676,21.76-21.658V34.573
+                                                                C511.86,22.561,502.126,12.827,490.114,12.813z M229.318,401.362l4.335,2.381c10.897,6.093,24.614,3.266,32.213-6.639
+                                                                l23.45-30.148c2.666-3.396,7.49-4.179,11.093-1.801l80.546,53.692c3.659,2.439,4.759,7.321,2.5,11.093l-20.198,33.673
+                                                                c-4.218,7.233-11.071,12.553-19.123,14.848c-40.337,11.093-110.933,1.707-218.078-105.446S9.56,195.273,20.627,154.97
+                                                                c2.293-8.051,7.61-14.903,14.839-19.123l33.673-20.207c3.773-2.254,8.652-1.155,11.093,2.5l53.717,80.546
+                                                                c2.382,3.602,1.599,8.43-1.801,11.093l-30.157,23.458c-9.903,7.597-12.731,21.311-6.639,32.205l2.389,4.335
+                                                                c8.533,15.65,19.14,35.123,57.805,73.779C194.212,382.213,213.677,392.828,229.318,401.362z M494.808,298.526
+                                                                c-0.028,2.567-2.127,4.627-4.693,4.608H141.203c-11.083-12.674-20.64-26.604-28.476-41.506l-2.458-4.48
+                                                                c-1.96-3.54-1.022-7.982,2.202-10.428l30.157-23.458c10.43-8.17,12.833-22.982,5.521-34.031l-20.275-30.43V34.573
+                                                                c-0.014-1.249,0.476-2.451,1.359-3.334c0.883-0.883,2.085-1.373,3.334-1.359h357.547c1.249-0.014,2.451,0.476,3.334,1.359
+                                                                c0.883,0.883,1.373,2.085,1.359,3.334V298.526z"/>
+                                                            <path d="M460.725,52.323l-142.618,108.16c-4.035,2.932-9.499,2.932-13.534,0L162.008,52.323
+                                                                c-3.756-2.849-9.111-2.113-11.959,1.643c-2.849,3.756-2.113,9.111,1.643,11.959l142.583,108.151
+                                                                c10.144,7.494,23.989,7.494,34.133,0L471.034,65.925c1.805-1.368,2.992-3.398,3.299-5.642c0.307-2.244-0.29-4.518-1.661-6.321
+                                                                C469.824,50.213,464.478,49.48,460.725,52.323z"/>
+                                                            <path d="M238.517,174.793l-87.415,93.611c-3.214,3.447-3.025,8.848,0.422,12.062c3.447,3.214,8.848,3.025,12.062-0.422
+                                                                l87.416-93.653c2.888-3.484,2.553-8.617-0.762-11.698C246.924,171.612,241.78,171.656,238.517,174.793z"/>
+                                                            <path d="M384.728,174.793c-3.214-3.447-8.614-3.637-12.062-0.422c-3.447,3.214-3.637,8.614-0.422,12.062l87.39,93.611
+                                                                c3.214,3.447,8.614,3.637,12.062,0.422c3.447-3.214,3.637-8.614,0.422-12.062L384.728,174.793z"/>
+                                                        </g>
+                                                    </g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                        </svg>  -->
+                                        <i class="fa fa-mobile" aria-hidden="true"></i>
+                                    </li>
+                                    <li class="footer__list">
+                                        <a href="tel:08040936034" class="footer__link">+080 – 4093 6034</a>
+                                    </li>
+                                </ul>
+                                <ul class="d-flex pb-3">
+                                    <li class="footer__list pr-3">
+                                        <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                    </li>
+                                    <li class="footer__list">
+                                        <a href="emaito:gis@gopalanschool.com" class="footer__link text-lowercase">gis@gopalanschool.com</a>
+                                    </li>
+                                </ul>
+                                <ul class="d-flex">
+                                    <li class="footer__list pr-3">
+                                        <i class="fa fa-map-marker" aria-hidden="true"></i> 
+                                    </li>
+                                    <li class="footer__list">
+                                        <address class=" m-0">
+                                            Behind SAP Labs, Seetharam Palya, Basavanagar, Hoodi, Bangalore -560 048
+                                        </address>
+                                    </li>
+                                </ul>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+                <!-- end footer middle -->
+            </footer>
+            
+
+            <div class="footer__copy-right">
+                <div class="wrap__container">
+                    <p>
+                        All rights reserved | Gopalan International School &copy;<script>document.write(new Date().getFullYear());</script> | <a href="index.html">gopalan international school</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- end body footer -->
+
+        <div class="section__modal">
+            <!-- Modal -->
+            <div class="modal fade" id="schedule-tour" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Schedule a Tour</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <form action="" method="POST" class="px-4">
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <label for="p_name">Parent Name</label>
+                                    <input name="p_name" type="type" class="form-control" id="p_name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="s_name">Student Name</label>
+                                    <input name="s_name" type="type" class="form-control" id="s_name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input name="email" type="email" class="form-control" id="email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="phone">Phone</label>
+                                    <input name="phone" type="tel" class="form-control" id="phone">
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" name="submit" class="btn btn__primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end body wrapper -->
+
+    <!-- script -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>    
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/scrollify/1.0.4/jquery.scrollify.min.js"></script> -->
+
+    <!-- custom js script -->
+    <script src="js/script.js"></script>    
+</body>
+</html>
