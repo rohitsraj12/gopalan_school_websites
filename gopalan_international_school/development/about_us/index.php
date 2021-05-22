0e2628398efeb39268f6124fdf87348c7140ad3f
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-22 23:27:06 +0530

    Create index.php

---
+++ gopalan_international_school/development/about_us/index.php
@@ -0,0 +1,14 @@
+
+<?php
+    $page_title = "About Us";
+    $meta_description = "";
+    $meta_keyword= "";
+
+
+    include "../config/config.php";
+
+    include "../include/header.inc.php";
+
+    include "../include/home_banner.inc.php";
+
+?>
