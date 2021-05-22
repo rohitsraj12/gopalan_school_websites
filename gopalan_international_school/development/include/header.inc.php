bbc61172dd7e58db8979313e7653988be0540a14
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-22 23:27:20 +0530

    Create header.inc.php

---
+++ gopalan_international_school/development/include/header.inc.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title;?> | Gopalan International School</title>
+    <meta name="description" content="<?php echo $meta_description;?>">
+    <meta name="keywords" content="<?php echo $meta_keyword;?>">
+    <meta name="author" content="Rohit">
+    <meta name="distribution" content="global" />
+    <meta http-equiv="content-language" content="en-gb">
+    <meta name="city" content="bangalore" />
+    <meta name="geo.region" content="IN" />
+    <meta name="state" content="Karnataka" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:creator" content="@"/>
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:keywords" content="">
+    
+    <!-- open graph code begins-->
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content=" ">
+    <meta property="og:url" content="">
+    <meta property="business:contact_data:street_address" content="Richmond Road">
+    <meta property="business:contact_data:locality" content="Bangalore">
+    <meta property="business:contact_data:region" content="Karnataka">
+    <meta property="business:contact_data:postal_code" content="560025">
+    <meta property="business:contact_data:country_name" content="India">
+    <link rel="shortcut icon" type="image/png" href="img/brand/fevicon.png" />
+    
+    <!-- frame-work/plug-in stylesheet -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css"> -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <!-- custom stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/plugIn.css">
+
+
+</head>
+<body id="capture-url" data-url="<?php echo $url;?>">
+
+    <div class="body__wrap">
+        <div class="body__header">
+            <div class="hamberger" id="hamberger">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </div>
+            <div class="body__header-top">
+                <div class="wrap__container">
+                    <div class="row justify-content-md-between">
+                        <div class="col-md-6">
+                            <address class=" m-0">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i> Behind SAP Labs, Seetharam Palya, Basavanagar, Hoodi, Bangalore -560 048
+                            </address>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="text-right d-md-flex justify-content-md-end">
+                                <li>
+                                    <a href="testimonial.html">Testimonial</a>
+                                    <!-- <a href="mail:gis@gopalanschool.com"><i class="fa fa-envelope-o" aria-hidden="true"></i> gis@gopalanschool.com</a> -->
+                                </li>
+                                <li class="ml-3">
+                                    <a href="faq.html">FAQ</a>
+                                    <!-- <a href="tel:08040936034"><i class="fa fa-phone-square" aria-hidden="true"></i> +080 – 4093 6034</a> -->
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- body header top -->
+
+            <header class="body__header-main">
+
+                    <div class="body_header-main-top">
+                        <div class="wrap__container d-md-flex justify-content-between align-items-end align-items-center">
+                            <div class="header__main-top-left">
+                                <div class="header__brand py-3">
+                                    <a href="index.html">
+                                        <img src="img/brand/gopalan_international_school_top.svg" alt="">
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="header__main-top-right">
+                                <ul class="d-md-flex">
+                                    <li class="d-flex pr-5 align-items-center">
+                                        <div class="left pr-3">
+                                            <svg version="1.1" id="Capa_1" class="svg theme" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                                viewBox="0 0 511.874 511.874" style="enable-background:new 0 0 511.874 511.874;" xml:space="preserve">
+                                                <g>
+                                                    <g>
+                                                        <g>
+                                                            <path d="M490.114,12.813H132.568c-12.012,0.014-21.746,9.748-21.76,21.76v98.62l-16.35-24.525
+                                                                c-7.484-11.289-22.535-14.676-34.133-7.68l-33.638,20.224c-11.016,6.464-19.097,16.946-22.545,29.244
+                                                                c-12.271,44.681-3.166,121.66,109.824,234.667C203.821,474.885,270.816,499.06,316.99,499.06
+                                                                c10.69,0.049,21.339-1.34,31.659-4.13c12.293-3.448,22.775-11.518,29.252-22.519l20.198-33.673
+                                                                c6.968-11.589,3.584-26.609-7.68-34.091l-80.546-53.692c-11.049-7.308-25.859-4.905-34.031,5.521l-23.45,30.148
+                                                                c-2.451,3.226-6.897,4.166-10.445,2.21l-4.463-2.458c-14.686-8.004-32.964-17.971-69.879-54.886
+                                                                c-3.994-3.994-7.612-7.731-11.008-11.307h333.517c11.982,0.009,21.713-9.676,21.76-21.658V34.573
+                                                                C511.86,22.561,502.126,12.827,490.114,12.813z M229.318,401.362l4.335,2.381c10.897,6.093,24.614,3.266,32.213-6.639
+                                                                l23.45-30.148c2.666-3.396,7.49-4.179,11.093-1.801l80.546,53.692c3.659,2.439,4.759,7.321,2.5,11.093l-20.198,33.673
+                                                                c-4.218,7.233-11.071,12.553-19.123,14.848c-40.337,11.093-110.933,1.707-218.078-105.446S9.56,195.273,20.627,154.97
+                                                                c2.293-8.051,7.61-14.903,14.839-19.123l33.673-20.207c3.773-2.254,8.652-1.155,11.093,2.5l53.717,80.546
+                                                                c2.382,3.602,1.599,8.43-1.801,11.093l-30.157,23.458c-9.903,7.597-12.731,21.311-6.639,32.205l2.389,4.335
+                                                                c8.533,15.65,19.14,35.123,57.805,73.779C194.212,382.213,213.677,392.828,229.318,401.362z M494.808,298.526
+                                                                c-0.028,2.567-2.127,4.627-4.693,4.608H141.203c-11.083-12.674-20.64-26.604-28.476-41.506l-2.458-4.48
+                                                                c-1.96-3.54-1.022-7.982,2.202-10.428l30.157-23.458c10.43-8.17,12.833-22.982,5.521-34.031l-20.275-30.43V34.573
+                                                                c-0.014-1.249,0.476-2.451,1.359-3.334c0.883-0.883,2.085-1.373,3.334-1.359h357.547c1.249-0.014,2.451,0.476,3.334,1.359
+                                                                c0.883,0.883,1.373,2.085,1.359,3.334V298.526z"/>
+                                                            <path d="M460.725,52.323l-142.618,108.16c-4.035,2.932-9.499,2.932-13.534,0L162.008,52.323
+                                                                c-3.756-2.849-9.111-2.113-11.959,1.643c-2.849,3.756-2.113,9.111,1.643,11.959l142.583,108.151
+                                                                c10.144,7.494,23.989,7.494,34.133,0L471.034,65.925c1.805-1.368,2.992-3.398,3.299-5.642c0.307-2.244-0.29-4.518-1.661-6.321
+                                                                C469.824,50.213,464.478,49.48,460.725,52.323z"/>
+                                                            <path d="M238.517,174.793l-87.415,93.611c-3.214,3.447-3.025,8.848,0.422,12.062c3.447,3.214,8.848,3.025,12.062-0.422
+                                                                l87.416-93.653c2.888-3.484,2.553-8.617-0.762-11.698C246.924,171.612,241.78,171.656,238.517,174.793z"/>
+                                                            <path d="M384.728,174.793c-3.214-3.447-8.614-3.637-12.062-0.422c-3.447,3.214-3.637,8.614-0.422,12.062l87.39,93.611
+                                                                c3.214,3.447,8.614,3.637,12.062,0.422c3.447-3.214,3.637-8.614,0.422-12.062L384.728,174.793z"/>
+                                                        </g>
+                                                    </g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                                <g>
+                                                </g>
+                                            </svg>
+                                        </div>
+                                        <div class="right">
+                                            <span><a href="mail:gis@gopalanschool.com"> gis@gopalanschool.com</a></span>    
+                                            <span><a href="tel:08040936034"> 080 4093 6034</a></span>
+                                        </div>
+                                    </li>
+                                    <li class="d-flex pr-5 align-items-center">
+                                        <div class="left pr-3">
+                                            <svg id="Icons" class="svg theme" height="40px" viewBox="0 0 74 74" xmlns="http://www.w3.org/2000/svg"><path d="m55.561 66.5a1 1 0 0 1 -.556-1.832c.872-.581 1.623-1.122 2.295-1.653a1 1 0 1 1 1.24 1.569c-.715.565-1.508 1.137-2.425 1.748a1 1 0 0 1 -.554.168z"/><path d="m37 72a34.97 34.97 0 1 1 32.55-22.111 1 1 0 1 1 -1.86-.738 32.959 32.959 0 1 0 -18.78 18.636 1 1 0 1 1 .72 1.866 34.962 34.962 0 0 1 -12.63 2.347z"/><path d="m63.02 59.88a1 1 0 0 1 -.763-1.647c.607-.716 1.211-1.5 1.846-2.4a1 1 0 0 1 1.634 1.154c-.67.948-1.309 1.778-1.954 2.54a1 1 0 0 1 -.763.353z"/><path d="m37 65.542a28.542 28.542 0 1 1 28.542-28.542 28.574 28.574 0 0 1 -28.542 28.542zm0-55.084a26.542 26.542 0 1 0 26.542 26.542 26.572 26.572 0 0 0 -26.542-26.542z"/><path d="m37 18a1 1 0 0 1 -1-1v-2a1 1 0 0 1 2 0v2a1 1 0 0 1 -1 1z"/><path d="m22.858 23.858a1 1 0 0 1 -.707-.293l-1.414-1.414a1 1 0 0 1 1.414-1.414l1.414 1.414a1 1 0 0 1 -.707 1.707z"/><path d="m17 38h-2a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2z"/><path d="m21.444 53.556a1 1 0 0 1 -.707-1.707l1.414-1.414a1 1 0 1 1 1.414 1.414l-1.414 1.414a1 1 0 0 1 -.707.293z"/><path d="m37 60a1 1 0 0 1 -1-1v-2a1 1 0 0 1 2 0v2a1 1 0 0 1 -1 1z"/><path d="m52.556 53.556a1 1 0 0 1 -.707-.293l-1.414-1.414a1 1 0 1 1 1.414-1.414l1.414 1.414a1 1 0 0 1 -.707 1.707z"/><path d="m59 38h-2a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2z"/><path d="m51.142 23.858a1 1 0 0 1 -.707-1.707l1.414-1.414a1 1 0 0 1 1.414 1.414l-1.414 1.414a1 1 0 0 1 -.707.293z"/><path d="m37 38.25a1 1 0 0 1 -.707-.293l-4.959-4.957a1 1 0 1 1 1.415-1.415l3.251 3.251v-12.836a1 1 0 0 1 2 0v15.25a1 1 0 0 1 -1 1z"/></svg>
+                                        </div>
+                                        <div class="right">
+                                            <span>07:30 - 19:00</span>    
+                                            <span>Monday to Friday</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <button class="btn__primary" data-toggle="modal" data-target="#schedule-tour">
+                                            <i class="fa fa-calendar-check-o pr-2" aria-hidden="true"></i>  Schedule a Tour
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="body__header-main-nav">
+                        <nav class="header__nav wrap__container">
+                            <ul class="d-md-flex">
+                                <li class="nav__list">
+                                    <a href="index.php" class="nav__link">home</a>
+                                </li>
+                                <li class="nav__list">
+                                    <a href="about_us.php" class="nav__link">About Us</a>
+                                </li>
+                                <li class="nav__list">
+                                    <a href="" class="nav__link">Adminsion</a>
+                                </li>
+                                <li class="nav__list">
+                                    <a href="" class="nav__link">Programmes</a>
+                                </li>
+                                <li class="nav__list">
+                                    <a href="" class="nav__link">Campus Life</a>
+                                </li>
+                                <li class="nav__list">
+                                    <a href="" class="nav__link">Gallery</a>
+                                </li>
+                                <li class="nav__list">
+                                    <a href="" class="nav__link">Events</a>
+                                </li>
+                                <li class="nav__list">
+                                    <a href="" class="nav__link">News</a>
+                                </li>
+                                <li class="nav__list">
+                                    <a href="" class="nav__link">Blog</a>
+                                </li>
+                                <li class="nav__list">
+                                    <a href="" class="nav__link">Contact Us</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+            </header>
+        </div>
+        <!-- end body header -->
