48ccaf3b4a28ce09cddf2aa53b7a327d6e10abaa
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-22 23:27:22 +0530

    Create home_banner.inc.php

---
+++ gopalan_international_school/development/include/home_banner.inc.php
@@ -0,0 +1,14 @@
+
+
+        <div class="body__banner">
+            <div class="desktop__banner">
+                <img src="img/banner/Slider.png" class="w-100" alt="">
+            </div>
+            <!-- end desktop banner -->
+
+            <div class="mobile__banner">
+                <img src="img/banner/Slider-sm.jpg" class="w-100" alt="">
+            </div>
+            <!-- end mobile banner -->
+        </div>
+        <!-- end body banner -->
